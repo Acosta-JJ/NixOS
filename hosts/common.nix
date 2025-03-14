51077097430b40652fa2be4ac40be62bf7f27755
Jose Acosta <acostasantana.jj@gmail.com>
2025-03-14 16:41:21 +0000

    Fix: change nixvim to neovim

--- hosts/common.nix
+++ hosts/common.nix
@@ -66,7 +66,7 @@ in {
       home.homeDirectory = "/home/${username}";
       home.stateVersion = "23.11"; # Please read the comment before changing.
       home.sessionVariables = {
-        EDITOR = "nvim";
+        EDITOR = "neovim";
         BROWSER = "firefox";
         TERMINAL = terminal;
       };
--- modules/programs/editor/neovim/default.nix
+++ modules/programs/editor/neovim/default.nix
@@ -4,8 +4,8 @@
       programs.neovim.enable = true;
       home.file.".config/nvim" = {
         source = builtins.fetchGit {
-          url = "https://github.com/Sly-Harvey/nvim.git";
-          rev = "018aad196d833c441ded5b09a8f8e7546d0c1bf1";
+          url = "https://github.com/acosta-JJ/nvim.git";
+          rev = "HEAD";
         };
         recursive = true;
       };
